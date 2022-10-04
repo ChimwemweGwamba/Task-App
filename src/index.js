431ad3fb2f52445ffb7826f522660f2142022cde
Chimwemwe <chimwemwe@limalinks.co.zm>
2022-10-04 06:20:57 +0200

    add json web tokens to user auth

--- src/index.js
+++ src/index.js
@@ -15,4 +15,3 @@ app.listen(port , () => {
     console.log('server is running on ' + port)
 })
 
-
--- src/routers/user.js
+++ src/routers/user.js
@@ -6,12 +6,24 @@ const User = require('../models/user')
 // Add User
 router.post('/users', async (req, res)  => {
     const user = new User(req.body)
-
+    
     try {
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
     } catch (e) {
         res.status(400).send(e)
+    }
+})
+
+
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ token, user })
+    } catch (e) {
+        res.status(400).send()
     }
 })
 
